3ebdd18d935342cb951db38c9d3a771c00542e20
Mohitinsan <mohithnd1055@gmail.com>
2025-08-15 23:02:41 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd 
+st.header(':rainbow[HOME PAGE]')
+st.write("___hello everyone this is my capstone project in year 2025 subjected to OpenLearn Organisation___")
+st.header("DATASET USED")
+st.write("___hello everyone this is my capstone project in year 2025 subjected to OpenLearn Organisation___")
+st.header("MODELS USED")
+st.write("___hello everyone this is my capstone project in year 2025 subjected to OpenLearn Organisation___")
+st.header("THE END")
