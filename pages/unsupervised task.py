725f4b6db48eeb05990d4344bed187ee90e699ee
Mohitinsan <mohithnd1055@gmail.com>
2025-08-15 23:03:32 +0530

    Add files via upload

---
+++ pages/unsupervised task.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd 
+st.header(':rainbow[unsupervised task]')
+st.write(":red[building....]")
+st.header("METRICS")
+st.write(":red[building....]")
+st.header("THE END")
